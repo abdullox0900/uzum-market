705b257fe4340db1c23d4e6269997414f765f789
Abdullaev008 <aabdullaev.web@gmail.com>
2023-07-01 18:47:06 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Header from './components/header/header'
 import Hero from './components/hero/hero'
+import Footer from './components/footer/footer'
+
 function App() {
 
   return (
     <>
     <Header/>
     <Hero/>
+    <h1>Hello</h1>
+    <Footer/>
+
     </>
   )
 }
