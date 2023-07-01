bd23095b2b2d24be25b19a134b88d256eb7aaa85
Behzod <kavolerahmedov@gmail.com>
2023-07-01 17:49:27 +0500

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ function App() {
 
   return (
     <>
-    <h1>Salom Azizbek, Abdulloh</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nobis rem cupiditate! Totam vero sed commodi blanditiis velit, obcaecati repudiandae, et ab quibusdam quam cupiditate porro consectetur libero molestiae! Odit?</p>
+    
     </>
   )
 }
