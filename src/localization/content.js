7cd261e6a1d6f7756ca3dd9532573e51a746191c
abdullox0900 <abdullox0900@gmail.com>
2023-07-04 19:27:21 +0500

    feat: add localization file and card component

---
+++ src/localization/content.js
@@ -0,0 +1,18 @@
+export const content = {
+    uz: {
+        city: 'Shahar',
+        tk: '',
+        savol_javoblar: 'Savol-javoblar'
+
+    },
+    ru: {
+        city: 'Gorod',
+        tk: '',
+        savol_javoblar: 'Вопросы и ответы'
+    },
+    en: {
+        city: 'City',
+        tk: '',
+        savol_javoblar: 'Q & A'
+    }
+}
