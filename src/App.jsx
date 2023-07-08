710d129b2db26ff8560bb0292e17e0dd732ec5c2
Behzod <kavolerahmedov@gmail.com>
2023-07-08 18:04:45 +0500

    footer changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/header'
 import Hero from './components/hero/hero'
-// import Footer from './components/footer/footer'
+import Footer from './components/footer/footer'
 import Slider from './components/slider/slider'
 // import Test from './components/test/test'
 import Card from './components/card/card'
@@ -19,6 +19,7 @@ function App() {
       <Hero lang={lang} setLang={setLang} />
       <Slider />
       <Card />
+      <Footer/>
     </>
   );
 }
